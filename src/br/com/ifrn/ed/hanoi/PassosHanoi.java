47c0741c9b2921c3de076960bc25d15e89c1ee1a
herlanassis <herlanassis@gmail.com>
2016-08-28 15:43:35 -0300

    alteração da classe de passos

--- src/br/com/ifrn/ed/hanoi/PassosHanoi.java
+++ src/br/com/ifrn/ed/hanoi/PassosHanoi.java
@@ -4,20 +4,19 @@ import br.com.ifrn.ed.hanoi.stack.MyStack;
 
 /**
  * Esta classe guarda o passo a passo da solução da torre de hanoi;
+ *
  * @author Herlan & Sávio
  */
 public class PassosHanoi {
 
     private MyStack<Integer> stackA, stackB, stackC;
     private long totalMovimentosRealizados;
-    private long totalMovimentosNecessarios;
 
-    public PassosHanoi(MyStack<Integer> stackA, MyStack<Integer> stackB, MyStack<Integer> stackC, long totalMovimentosRealizados, long totalMovimentosNecessarios) {
+    public PassosHanoi(MyStack<Integer> stackA, MyStack<Integer> stackB, MyStack<Integer> stackC, long totalMovimentosRealizados) {
         this.stackA = stackA;
         this.stackB = stackB;
         this.stackC = stackC;
         this.totalMovimentosRealizados = totalMovimentosRealizados;
-        this.totalMovimentosNecessarios = totalMovimentosNecessarios;
     }
 
     public MyStack<Integer> getStackA() {
@@ -35,9 +34,4 @@ public class PassosHanoi {
     public long getTotalMovimentosRealizados() {
         return totalMovimentosRealizados;
     }
-
-    public long getTotalMovimentosNecessarios() {
-        return totalMovimentosNecessarios;
-    }
-
 }
